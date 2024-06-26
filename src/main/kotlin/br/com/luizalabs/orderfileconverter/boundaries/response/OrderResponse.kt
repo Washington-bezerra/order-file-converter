c441bf0ab0eb0efe329b38e1a0622f775dbdeabf
Washington <luiswashington00@gmail.com>
2024-06-25 22:42:18 -0300

    style: add @JsonProperty to variable with underscore

--- src/main/kotlin/br/com/luizalabs/orderfileconverter/boundaries/response/OrderResponse.kt
+++ src/main/kotlin/br/com/luizalabs/orderfileconverter/boundaries/response/OrderResponse.kt
@@ -1,17 +1,17 @@
 package br.com.luizalabs.orderfileconverter.boundaries.response
 
-import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.math.BigDecimal
 
 data class OrderConverterResponse(
-    @JsonAlias("user_id")
+    @JsonProperty("user_id")
     val userId: Int,
     val name: String,
     val orders: List<OrderResponse>
 )
 
 data class OrderResponse(
-    @JsonAlias("order_id")
+    @JsonProperty("order_id")
     val orderId: Int,
     val total: BigDecimal,
     val date: String,
@@ -19,7 +19,7 @@ data class OrderResponse(
 )
 
 data class ProductResponse(
-    @JsonAlias("product_id")
+    @JsonProperty("product_id")
     val productId: Int,
     val value: BigDecimal
 )
